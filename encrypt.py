2ef0a4154e4e22154dad519ad15848e9ac84c2cc
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2023-07-10 13:58:48 +0100

    Create encrypt.py
    encrypt plain-text message to cipher-text, using own alphabet, with a randomly generated key.

---
+++ encrypt.py
@@ -0,0 +1,51 @@
+# From https://www.youtube.com/watch?v=vsLBErLWBhA
+# ----------------------------
+# run with $ python3 encrypt.py
+# A routine for python3
+# Composed in IDLE (3.6.9) IDE
+# Author: Mitch Bayliss
+# E-mail: mitch@ur3.uk
+# Date: 9 Jul 2023
+# Licence: Creative Commons (CC)
+#-------------------------------
+
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+# alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# alpha = list(alpha)
+# random.shuffle(alpha)
+# print(alpha)
+# LFUWGHXBOAYKEITPMDVNCJQZSR
+
+#ENCRYPT
+# The user, types in a plain-text message.
+plain_text = input("Enter a message to encrypt: ")
+
+# This is where the cipher-text will be held.
+cipher_text = ""
+
+# loop for each letter and space, in the message.
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message : {plain_text}")
+print(f"encrypted message: {cipher_text}")
+
+#DECRYPT
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message : {plain_text}")
